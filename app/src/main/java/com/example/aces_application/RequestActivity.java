0ea2a90d4b3872933e0ac77c9c251c15acbe07db
laurenjohnson13 <laurenjohnson13@augustana.edu>
2017-01-21 16:53:23 -0600

    added view pager,pop up window, and request form layout

--- app/src/main/java/com/example/aces_application/RequestActivity.java
+++ app/src/main/java/com/example/aces_application/RequestActivity.java
@@ -1,13 +1,60 @@
 package com.example.aces_application;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
 
 public class RequestActivity extends AppCompatActivity {
+
+    private static final String database = "ZeXbN6PpTgc9wWQOCk7Nju5G0B92";
+    private int numRiders;
+    private List<Integer> id;
+    private String location;
+    private String destination;
+    private LinearLayout linearLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
+
+        linearLayout = (LinearLayout) findViewById(R.id.linearList);
+
+        Bundle bundle = getIntent().getExtras();
+        numRiders = bundle.getInt("numRiders");
+
+        fillIdList();
+
+    }
+
+    private void fillIdList() {
+        EditText id;
+
+        for (int i = 0; i < numRiders; i++) {
+            //STORE THE INDIVIDUAL PAINTINGS AS BUTTONS
+            id = new EditText(RequestActivity.this);
+
+
+            //USE THE CONTENT DESCRIPTION PROPERTY TO STORE
+            //ID DATA
+            id.setContentDescription("id" + i);
+            id.setTag(i);
+
+
+            //ADD THE EDIT TEXT TO THE LINEAR LIST
+            linearLayout.addView(id);
+        }
+
     }
 }
--- app/src/main/java/com/example/aces_application/WelcomeActivity.java
+++ app/src/main/java/com/example/aces_application/WelcomeActivity.java
@@ -1,13 +1,104 @@
 package com.example.aces_application;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Color;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.GRAY;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    CustomSwipeAdapter customSwipeAdapter;
+    Timer timer;
+    int page;
+    Button requestBtn;
+    int numRiders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        requestBtn = (Button) findViewById(R.id.requestBtn);
+        requestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(WelcomeActivity.this);
+
+                alert.setTitle("Amount");
+                alert.setMessage("How many riders are there?");
+
+                // Set an EditText view to get user input
+                final EditText input = new EditText(WelcomeActivity.this);
+                alert.setView(input);
+
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        numRiders = Integer.parseInt(input.getText().toString());
+                        Bundle bundle = new Bundle();
+                        bundle.putInt("numRiders",numRiders);
+                        Intent intent = new Intent(WelcomeActivity.this, RequestActivity.class);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Canceled.
+                    }
+                });
+
+                alert.show();
+
+            }
+        });
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        customSwipeAdapter = new CustomSwipeAdapter(this);
+        viewPager.setAdapter(customSwipeAdapter);
+        page = 0;
+        pageSwitcher(5);
+    }
+
+    public void pageSwitcher(int seconds) {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new RemindTask(), 0, seconds * 1000);
+    }
+
+    // this is an inner class...
+    class RemindTask extends TimerTask {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    if (page == 0) {
+                        viewPager.setCurrentItem(0);
+                        page++;
+                    }else if (page >= customSwipeAdapter.getCount()) {
+                        page = 0;
+                        viewPager.setCurrentItem(page);
+                    } else {
+                        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                        page = viewPager.getCurrentItem() + 1;
+                    }
+                }
+            });
+
+        }
     }
 }
