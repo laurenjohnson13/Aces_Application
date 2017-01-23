2c32a9b28221ec33393bbd3bb773d14adb7df76c
laurenjohnson13 <laurenjohnson13@augustana.edu>
2017-01-23 16:04:37 -0600

    request layout fixed

--- app/src/main/java/com/example/aces_application/RequestActivity.java
+++ app/src/main/java/com/example/aces_application/RequestActivity.java
@@ -1,35 +1,60 @@
 package com.example.aces_application;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RequestActivity extends AppCompatActivity {
 
-    private static final String database = "ZeXbN6PpTgc9wWQOCk7Nju5G0B92";
+    private static final String mDatabase = "ZeXbN6PpTgc9wWQOCk7Nju5G0B92";
     private int numRiders;
     private List<Integer> id;
-    private String location;
-    private String destination;
+    private List<EditText> idText;
+    private EditText location;
+    private EditText destination;
     private LinearLayout linearLayout;
+    private Button submitBtn;
+    private DatabaseReference database;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
-
-        linearLayout = (LinearLayout) findViewById(R.id.linearList);
+        database = FirebaseDatabase.getInstance().getReference();
+        linearLayout = (LinearLayout) findViewById(R.id.linear_layout);
+        submitBtn = (Button) findViewById(R.id.requestBtn);
+        location = (EditText) findViewById(R.id.editText1);
+        destination = (EditText) findViewById(R.id.editText2);
+        id = new ArrayList<Integer>();
+        idText = new ArrayList<EditText>();
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addInfoToFirebase();
+                Intent intent = new Intent(RequestActivity.this, WaitActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Bundle bundle = getIntent().getExtras();
         numRiders = bundle.getInt("numRiders");
@@ -38,22 +63,37 @@ public class RequestActivity extends AppCompatActivity {
 
     }
 
-    private void fillIdList() {
-        EditText id;
+    private void addInfoToFirebase() {
+        String mLocation = location.getText().toString();
+        String mDestination = destination.getText().toString();
+        for(int i = 0; i < numRiders; i++) {
+            EditText mId = idText.get(i);
+            id.add(Integer.parseInt(mId.getText().toString()));
+        }
 
+        Ride newRide = new Ride(mLocation, mDestination, id, true);
+        database.child("Ride").push().setValue(newRide);
+
+    }
+
+    private void fillIdList() {
+        EditText mId;
         for (int i = 0; i < numRiders; i++) {
             //STORE THE INDIVIDUAL PAINTINGS AS BUTTONS
-            id = new EditText(RequestActivity.this);
+             mId = new EditText(RequestActivity.this);
 
 
             //USE THE CONTENT DESCRIPTION PROPERTY TO STORE
             //ID DATA
-            id.setContentDescription("id" + i);
-            id.setTag(i);
+            mId.setContentDescription("id" + i);
+            mId.setTag(i);
+            mId.requestFocus();
 
 
             //ADD THE EDIT TEXT TO THE LINEAR LIST
-            linearLayout.addView(id);
+            linearLayout.addView(mId);
+            idText.add(mId);
+
         }
 
     }
--- app/src/main/java/com/example/aces_application/WaitActivity.java
+++ app/src/main/java/com/example/aces_application/WaitActivity.java
@@ -1,13 +1,35 @@
 package com.example.aces_application;
 
+import android.os.CountDownTimer;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WaitActivity extends AppCompatActivity {
+
+    private CountDownTimer timer;
+    private TextView timerText;
+    private DatabaseReference database;
+    private int activeRides;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wait);
+
+        Bundle bundle = getIntent().getExtras();
+        activeRides = bundle.getInt("activeRiders");
+
+        timerText = (TextView) findViewById(R.id.text2);
+        timerText.setText("" + activeRides);
+
+        //TODO: start screen and timer/cancel/picked up
     }
 }
